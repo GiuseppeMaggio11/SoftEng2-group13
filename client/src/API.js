8a3478c26669abe3a5b7ed82c79dd832d5644f1a
Paolo Campagnale s317687 <s317687@studenti.polito.it>
2023-10-21 18:57:14 +0200

    Added reset queries count API call for the client

--- client/src/API.js
+++ client/src/API.js
@@ -1,28 +1,54 @@
-//const SERVER_URL = 'http://localhost:3001/api/';
+const SERVER_URL = 'http://localhost:3001/api/';
 
 function getJson(httpResponsePromise) {
-    // server API always return JSON, in case of error the format is the following { error: <message> } 
-    return new Promise((resolve, reject) => {
-      httpResponsePromise
-        .then((response) => {
-          if (response.ok) {
-  
-           // the server always returns a JSON, even empty {}. Never null or non json, otherwise the method will fail
-           response.json()
-              .then( json => resolve(json) )
-              .catch( err => reject({ error: "Cannot parse server response" }))
-  
-          } else {
-            // analyzing the cause of error
-            response.json()
-              .then(obj => 
-                reject(obj)
-                ) // error msg in the response body
-              .catch(err => reject({ error: "Cannot parse server response" })) // something else
-          }
-        })
-        .catch(err => 
-          reject({ error: "Cannot communicate"  })
-        ) // connection error
-    });
-  }
+  // server API always return JSON, in case of error the format is the following { error: <message> } 
+  return new Promise((resolve, reject) => {
+    httpResponsePromise
+      .then((response) => {
+        if (response.ok) {
+
+          // the server always returns a JSON, even empty {}. Never null or non json, otherwise the method will fail
+          response.json()
+            .then(json => resolve(json))
+            .catch(err => reject({ error: "Cannot parse server response" }))
+
+        } else {
+          // analyzing the cause of error
+          response.json()
+            .then(obj =>
+              reject(obj)
+            ) // error msg in the response body
+            .catch(err => reject({ error: "Cannot parse server response" })) // something else
+        }
+      })
+      .catch(err =>
+        reject({ error: "Cannot communicate" })
+      ) // connection error
+  });
+}
+
+
+
+async function resetQueuesCounter() {
+  return new Promise((resolve, reject) => {
+    fetch(SERVER_URL + "/reset", {
+      method: 'PUT'
+    }).then((response) => {
+      if (response.ok) {
+        resolve(null);
+      } else {
+        response.json()
+          .then((message) => { reject(message); })
+          .catch(() => { reject({ error: "Cannot parse server response." }) });
+      }
+    }).catch(() => { reject({ error: "Cannot communicate with the server." }) });
+  });
+}
+
+
+
+const API = {
+  resetQueuesCounter
+};
+
+export default API;
