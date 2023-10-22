f9bd717ea39068d3d8f017a6d7bcc02a02f5f243
Manuel Fissore <s269523@studenti.polito.it>
2023-10-22 19:55:56 +0200

    route sketch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
-
-import { useState } from 'react'
+import React from 'react'; 
+import { BrowserRouter, Outlet, Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 import './App.css'
+import './routes/Customer'
+import Customer from './routes/Customer';
+import Home from './routes/Home';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home/>} />
+        <Route path="/customer" element={<Customer/>} />
+        {/*add a new path here */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h2>This is the Home page.</h2>
+      <Link to="/customer"><button>Customer Interface</button></Link>
+       {/*add other buttons*/}
+    </div>
+  );
+}
+
+
+export default Home;
