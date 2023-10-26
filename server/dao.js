6c1779aee786f4f6daf4cb6e28823c0c6c132a1c
Paolo Campagnale s317687 <s317687@studenti.polito.it>
2023-10-26 14:07:49 +0200

    Add unit and integration test for US5

--- server/dao.js
+++ server/dao.js
@@ -5,82 +5,86 @@
 const sqlite = require("sqlite3");
 
 // Open the database
-const db = new sqlite.Database("database.db", (err) => {
-  if (err) throw err;
+const db = new sqlite.Database(process.env.TEST_DB ? ":memory:" : "database.db", (err) => {
+    if (err) throw err;
 });
+
+exports.getDatabaseInstance = () => {
+    return db;
+};
 
 /***QUERY***/
 
 // get ticket number
 exports.getTicketNumber = (queueName) => {
-  return new Promise((resolve, reject) => {
-    const sql = "SELECT MIN(ticketNumber) AS minTicketNumber FROM queues WHERE queue = ? ";
-    db.get(sql, [queueName], (err, row) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      console.log(row)
-      const count = { count: row.minTicketNumber};
-      resolve(count);
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT MIN(ticketNumber) AS minTicketNumber FROM queues WHERE queue = ? ";
+        db.get(sql, [queueName], (err, row) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            console.log(row)
+            const count = { count: row.minTicketNumber };
+            resolve(count);
+        });
     });
-  });
 };
 
 exports.updateStatistics = (queueName) => {
-  console.log('f')
-  return new Promise((resolve, reject) => {
-      const today = new Date().toISOString().slice(0, 10); // Get current date in YYYY-MM-DD format
-      console.log('today' + today)
-      // Check if a row for the current date and queue exists
-      const checkQuery = `
+    console.log('f')
+    return new Promise((resolve, reject) => {
+        const today = new Date().toISOString().slice(0, 10); // Get current date in YYYY-MM-DD format
+        console.log('today' + today)
+        // Check if a row for the current date and queue exists
+        const checkQuery = `
           SELECT * FROM statistics
           WHERE date = ? AND queue = ?
-      `;
-      db.get(checkQuery, [today, queueName], (err, row) => {
-          if (err) {
-              reject(err);
-              return;
-          }
-          console.log(row)
-          if (row) {
-              // If a row exists, increment the amount by 1
-              const updateQuery = `
+      `.replace(/\s+/g, ' ').trim();
+        db.get(checkQuery, [today, queueName], (err, row) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            console.log(row)
+            if (row) {
+                // If a row exists, increment the amount by 1
+                const updateQuery = `
                   UPDATE statistics
                   SET amount = amount + 1
                   WHERE date = ? AND queue = ?
-              `;
-              db.run(updateQuery, [today, queueName], (err) => {
-                  if (err) {
-                      reject(err);
-                  } else {
-                      resolve('Row updated');
-                  }
-              });
-          } else {
-              // If no row exists, create a new row with value 1 in the count
-              const insertQuery = `
+              `.replace(/\s+/g, ' ').trim();
+                db.run(updateQuery, [today, queueName], (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve('Row updated');
+                    }
+                });
+            } else {
+                // If no row exists, create a new row with value 1 in the count
+                const insertQuery = `
                   INSERT INTO statistics (queue, amount, date)
                   VALUES (?, 1, ?)
-              `;
-              db.run(insertQuery, [queueName, today], (err) => {
-                  if (err) {
-                      reject(err);
-                  } else {
-                      resolve('New row created');
-                  }
-              });
-          }
-      });
-  });
+              `.replace(/\s+/g, ' ').trim();
+                db.run(insertQuery, [queueName, today], (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve('New row created');
+                    }
+                });
+            }
+        });
+    });
 }
 
 
 // update queue count
 exports.deleteServed = (name) => {
-  console.log(name)
-  return new Promise((resolve, reject) => {
-    const sql = `
+    console.log(name)
+    return new Promise((resolve, reject) => {
+        const sql = `
     DELETE FROM queues
     WHERE id = (
         SELECT id
@@ -89,28 +93,28 @@ exports.deleteServed = (name) => {
         ORDER BY ticketNumber
         LIMIT 1
     )
-`;
-    db.run(sql, [name], function (err) {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve({ numRowUpdated: this.changes });
+`.replace(/\s+/g, ' ').trim();
+        db.run(sql, [name], function (err) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({ numRowUpdated: this.changes });
+        });
     });
-  });
 }
 
 exports.getLastTicket = (queue) => {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const sql = 'SELECT MAX(ticketNumber) AS maxTicketNumber FROM queues WHERE queue = ?';
-        db.get(sql,[queue] ,(err, row) => {
+        db.get(sql, [queue], (err, row) => {
             if (err) {
-                reject (err);
+                reject(err);
                 return;
             } else {
                 console.log(row)
-                let count = {count : row.maxTicketNumber}
-                resolve (count);
+                let count = { count: row.maxTicketNumber }
+                resolve(count);
                 return;
             }
         })
@@ -118,13 +122,13 @@ exports.getLastTicket = (queue) => {
 }
 
 exports.addTicket = (queue, ticket) => {
-    return new Promise ((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const sql = 'INSERT INTO queues (queue, ticketNumber) VALUES (?,?)'
-        db.run(sql, [queue, ticket], function(err) {
-            if(err){
+        db.run(sql, [queue, ticket], function (err) {
+            if (err) {
                 reject(err);
                 return;
-            } 
+            }
             resolve();
             return;
         })
@@ -141,7 +145,7 @@ exports.getLastTicketAll = () => {
             }
             resolve(rows);
         });
-    }); 
+    });
 }
 
 exports.resetQueuesTotal = () => {
@@ -154,7 +158,7 @@ exports.resetQueuesTotal = () => {
             } else {
                 resolve(this.changes);
             }
-            
+
         });
-    }); 
+    });
 }
